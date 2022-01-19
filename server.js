d66a45413c02eb6d4c065c5e14707b515e0fe004
Alistair Houghton <alistair.g.houghton@gmail.com>
2022-01-19 13:57:27 +0000

    added files

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const apiRoute = require('./Routes/apiRoutes.js')
+const htmlRoute = require('./Routes/htmlRoutes.js')
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+
+//need to update
+
+app.use('/api',apiRoute)
+app.use('/', htmlRoute)
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+
