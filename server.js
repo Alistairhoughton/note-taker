a9eb26f34c9d121de2a43fb9d0c3d38227d3b469
Alistair Houghton <alistair.g.houghton@gmail.com>
2022-01-19 15:31:49 +0000

    sorted the routes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const apiRoute = require('./Routes/apiRoutes.js')
-const htmlRoute = require('./Routes/htmlRoutes.js')
+const apiRoute = require('./Routes/apiRoutes.js');
+const htmlRoute = require('./Routes/htmlRoutes.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
@@ -12,8 +13,8 @@ app.use(express.static('public'))
 
 //need to update
 
-app.use('/api',apiRoute)
-app.use('/', htmlRoute)
+app.use('/api',apiRoute);
+app.use('/', htmlRoute);
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 
