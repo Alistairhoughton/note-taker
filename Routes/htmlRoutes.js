b0b2935648d3baefc9d44278221ae47b98d9df5a
Alistair Houghton <alistair.g.houghton@gmail.com>
2022-01-21 18:19:33 +0000

    updated start script

--- Routes/htmlRoutes.js
+++ Routes/htmlRoutes.js
@@ -5,6 +5,7 @@ const path = require('path')
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
+
 module.exports = router
 
-res.render();
+// res.render();
